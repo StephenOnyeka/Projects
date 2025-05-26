a6766020e6f21d68cbef120020a5ec89ee694569
DonMizzy <ejeanobionyeka@gmail.com>
2025-05-26 20:07:55 +0100

    edited the page and added new blog

--- portfolio-nextjs/pages/index.js
+++ portfolio-nextjs/pages/index.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
-import HOme from "@/components/Home";
+// import HOme from "@/components/Home";
 // import Navbar from "@/components/Navbar";
+import ModernFullPreview from "@/components/Home";
 
 export default function Home() {
   return (
     <div>
       {/* <Navbar />  */}
-      <HOme />
+      {/* <HOme /> */}
+      <ModernFullPreview />
     </div>
   );
 }
