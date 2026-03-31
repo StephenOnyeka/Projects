a37c13c3e27638e8e5b0cf5840d0e2ed4fa6f9db
StephenOnyeka <ejeanobionyeka@gmail.com>
2026-04-01 00:13:10 +0100

    added the globe gif, and it's working

---
+++ portfolio-nextjs/components/Globe.js
@@ -0,0 +1,9 @@
+/*
+ * Globe (framer/Globe@0.7.0)
+ *
+ * Learn More: https://www.framer.com/asset-urls
+ */
+
+export * from "https://framerusercontent.com/modules/xFhv3ETdRBLjJusGaZ1P/SY4EOdo4KxEvLISnWiR3/Globe.js"
+export { default } from "https://framerusercontent.com/modules/xFhv3ETdRBLjJusGaZ1P/SY4EOdo4KxEvLISnWiR3/Globe.js"
+
