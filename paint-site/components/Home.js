bfdaa24bd1586f976dc36b88b8dd4d685a40f005
StephenOnyeka <ejeanobionyeka@gmail.com>
2026-09-04 12:40:42 +0100

    nothing

--- paint-site/components/Home.js
+++ paint-site/components/Home.js
@@ -48,7 +48,7 @@ function HoMe() {
             </p>
             <p className="text-4xl lg:text-5xl xl:text-6xl font-bold text-primary pb-4 ">
               {/* ProRange: <br /> */}
-              AdorationPaints: <br />
+              AdorationPaintszz: <br />
               premium painting company
             </p>
             <Link href="/services">
